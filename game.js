f702e0ca5b363f41df98bb3d65e0e7d48353f2fb
Hydeia <blakeyhydeia@gmail.com>
2021-12-06 18:33:12 -0500

    move request functionality to inside of the guess_that_quote page

--- game.js
+++ game.js
@@ -18,6 +18,102 @@ export const guessQuotePage = () => {
         window.location.href=`${quotePage}`
         //console.log("button was clicked");
         
+    let quoteData = []; 
+
+
+
+    const getQuoteData = async () => {
+            try {
+                    const response = await axios.get(`https://animechan.vercel.app/api/random`)
+                    // console.log(`Quote Data: ${response.data.quote}`); 
+                    console.log(response); 
+                    return response.data
+
+            } catch (err) {
+                    console.log(err)
+            }
+    }
+
+    const getChoice2 = async () => {
+            try {
+                    const response = await axios.get(`https://animechan.vercel.app/api/random`)
+                    // console.log(`Quote Data: ${response.data.quote}`); 
+                    console.log(response); 
+                    return response.data.character
+
+            } catch (err) {
+                    console.log(err)
+            }
+    }
+
+    const getChoice3 = async () => {
+            try {
+                    const response = await axios.get(`https://animechan.vercel.app/api/random`)
+                    // console.log(`Quote Data: ${response.data.quote}`); 
+                    console.log(response); 
+                    return response.data.character
+
+            } catch (err) {
+                    console.log(err)
+            }
+    }
+
+    const getChoice4 = async () => {
+            try {
+                    const response = await axios.get(`https://animechan.vercel.app/api/random`)
+                    // console.log(`Quote Data: ${response.data.quote}`); 
+                    console.log(response); 
+                    return response.data.character
+
+            } catch (err) {
+                    console.log(err)
+            }
+    }
+
+    let option1 = document.getElementById('choice1'); 
+    let option2 = document.getElementById('choice2'); 
+    let option3 = document.getElementById('choice3'); 
+    let option4 = document.getElementById('choice4'); 
+
+    const createQuote = async () => {
+
+    const quoteElement = await getQuoteData();
+    const choice2 = await getChoice2(); 
+    const choice3 = await getChoice3(); 
+    const choice4 = await getChoice4(); 
+
+    const parentP = document.getElementById('quoteAPI'); 
+    const paraEle = document.createElement('p'); 
+    const hintEle = document.createElement('p'); 
+
+    
+    //appending each quote to the who said that quote page. 
+    paraEle.append(`${quoteElement.quote}.` ); 
+    hintEle.append(`*Hint: this quote is from the anime ${quoteElement.anime}`)
+    
+    //pushing the quote character data to quotedata array
+    quoteData.push(quoteElement.character, choice2, choice3, choice4); 
+
+    //logging the data 
+    console.log('quote: ' + quoteElement.quote)
+    console.log('quote character data: ' + quoteData)
+
+    // Must randomize the choices -- Work on this later
+  
+    option1.innerHTML = `${quoteData[0]}`
+    option2.innerHTML = `${quoteData[1]}`
+    option3.innerHTML = `${quoteData[2]}`
+    option4.innerHTML = `${quoteData[3]}`
+    
+    parentP.appendChild(paraEle)
+    paraEle.appendChild(hintEle);
+
+
+    }
+createQuote();
+
+
+        
 }
 
 export const guessMangaPage = () => {
@@ -26,105 +122,17 @@ export const guessMangaPage = () => {
 
 }
 
-window.addEventListener('load',()=>{
+window.addEventListener('DOMContentLoaded', (event) => {
+        console.log('DOM fully loaded and parsed');
         guessAnimeBtn.addEventListener("click", guessAnimePage);
         guessQuoteBtn.addEventListener("click", guessQuotePage); 
         guessMangaBtn.addEventListener("click", guessMangaPage); 
-    })
+    });
+
+
 
 
     //use to save the character names from the quote API
 
-    let quoteData = []; 
-
-
-
-        const getQuoteData = async () => {
-                try {
-                        const response = await axios.get(`https://animechan.vercel.app/api/random`)
-                        // console.log(`Quote Data: ${response.data.quote}`); 
-                        console.log(response); 
-                        return response.data
-
-                } catch (err) {
-                        console.log(err)
-                }
-        }
-
-        const getChoice2 = async () => {
-                try {
-                        const response = await axios.get(`https://animechan.vercel.app/api/random`)
-                        // console.log(`Quote Data: ${response.data.quote}`); 
-                        console.log(response); 
-                        return response.data.character
-
-                } catch (err) {
-                        console.log(err)
-                }
-        }
-
-        const getChoice3 = async () => {
-                try {
-                        const response = await axios.get(`https://animechan.vercel.app/api/random`)
-                        // console.log(`Quote Data: ${response.data.quote}`); 
-                        console.log(response); 
-                        return response.data.character
-
-                } catch (err) {
-                        console.log(err)
-                }
-        }
-
-        const getChoice4 = async () => {
-                try {
-                        const response = await axios.get(`https://animechan.vercel.app/api/random`)
-                        // console.log(`Quote Data: ${response.data.quote}`); 
-                        console.log(response); 
-                        return response.data.character
-
-                } catch (err) {
-                        console.log(err)
-                }
-        }
-
-        let option1 = document.getElementById('choice1'); 
-        let option2 = document.getElementById('choice2'); 
-        let option3 = document.getElementById('choice3'); 
-        let option4 = document.getElementById('choice4'); 
-
-        const createQuote = async () => {
-
-        const quoteElement = await getQuoteData();
-        const choice2 = await getChoice2(); 
-        const choice3 = await getChoice3(); 
-        const choice4 = await getChoice4(); 
-
-        const parentP = document.getElementById('quoteAPI'); 
-        const paraEle = document.createElement('p'); 
-        const hintEle = document.createElement('p'); 
-   
-        paraEle.append(`${quoteElement.quote}.` ); 
-        hintEle.append(`*Hint: this quote is from the anime ${quoteElement.anime}`)
-
-        quoteData.push(quoteElement.character, choice2, choice3, choice4); 
-        console.log('quoteData: ' + quoteData)
-
-        // Must randomize the choices -- Work on this later
-
-        option1.innerHTML = `${quoteData[0]}`
-        option2.innerHTML = `${quoteData[1]}`
-        option3.innerHTML = `${quoteData[2]}`
-        option4.innerHTML = `${quoteData[3]}`
-
-        parentP.appendChild(paraEle)
-        paraEle.appendChild(hintEle); 
-
-
-    }
-
-
-
-
-    createQuote(); 
  
 
