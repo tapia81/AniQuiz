36663cb6185db166eaca32b859dc3d9518ed4e38
Hydeia <blakeyhydeia@gmail.com>
2021-12-10 20:24:36 -0500

    remove unused code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 import { guessAnimePage } from '/components/animePage.js';
 import { guessQuotePage } from '/components/quotePage.js';
 import { guessMangaPage } from '/components/mangaPage.js';
-let requestQuotes = 'https://animechan.vercel.app/api/random';
