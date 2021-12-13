aa0a46c1143e4cec8c8caeac6649876341415b72
Hydeia <blakeyhydeia@gmail.com>
2021-12-13 17:50:23 -0500

    fix hamburger issues

--- script.js
+++ script.js
@@ -2,14 +2,49 @@ import { guessAnimePage } from '/components/animePage.js';
 import { guessMangaPage } from '/components/mangaPage.js';
 import { guessQuotePage } from '/components/quotePage.js';
 
+
 $(document).ready(function() {
 	// Check for click events on the navbar burger icon
+
 	$('.navbar-burger').click(function() {
 		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
 		$('.navbar-burger').toggleClass('is-active');
 		$('.navbar-menu').toggleClass('is-active');
 	});
 });
+
+
+// let burgerLine = document.getElementsByClassName('navbar-burger')[0]
+// burgerLine.addEventListener('click', function () {
+// 	console.log('you are clicking me')
+// })
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+// 	// Get all "navbar-burger" elements
+// 	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+// 	// Check if there are any navbar burgers
+// 	if ($navbarBurgers.length > 0) {
+  
+// 	  // Add a click event on each of them
+// 	  $navbarBurgers.forEach( el => {
+// 		el.addEventListener('click', () => {
+  
+// 		  // Get the target from the "data-target" attribute
+// 		  const target = el.dataset.target;
+// 		  const $target = document.getElementById(target);
+  
+// 		  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+// 		  el.classList.toggle('is-active');
+// 		  $target.classList.toggle('is-active');
+  
+// 		});
+// 	  });
+// 	}
+  
+//   });
+
 
 // let currentImage = document.getElementsByClassName('slideshow-image');
 // let imageBox = document.getElementsByClassName('slideshow-box');
